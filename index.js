a9b2df8706bbc26dc16484a023429e3e832e2e92
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-28 20:40:28 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(actionWord="roller-skate"){
+  return `This Saturday, I want to ${actionWord}!`
+}
+
+function mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(highlight){
+    return function newfunc(adjective){
+      return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
